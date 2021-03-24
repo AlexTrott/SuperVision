1096904ec068fce3a0c1dc2b8e8ead867e492b9f
Alex Trott <hello@alextrott.me>
2021-03-24 18:50:47 +0000

    Adds core frameworks

--- Package.swift
+++ Package.swift
@@ -12,15 +12,19 @@ let package = Package(
             targets: ["SuperVision"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/mixpanel/mixpanel-swift.git", from: "2.0.0"),
+        .package(name: "Sentry", url: "https://github.com/getsentry/sentry-cocoa", from: "6.2.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SuperVision",
-            dependencies: []),
+            dependencies: [
+                "Mixpanel",
+                "Sentry"
+            ]
+        ),
         .testTarget(
             name: "SuperVisionTests",
             dependencies: ["SuperVision"]),

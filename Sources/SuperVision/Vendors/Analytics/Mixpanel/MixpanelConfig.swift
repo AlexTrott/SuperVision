483f75383de21ca9efa49fbba46298b7433de58c
Alex Trott <hello@alextrott.me>
2021-03-24 20:04:55 +0000

    Adds public init

--- Sources/SuperVision/Vendors/Analytics/Mixpanel/MixpanelConfig.swift
+++ Sources/SuperVision/Vendors/Analytics/Mixpanel/MixpanelConfig.swift
@@ -11,4 +11,14 @@ public struct MixpanelConfig: InitializableConfig {
     public let token: String
     public let serverURL: String
     public let loggingEnabled: Bool
+
+    public init(
+        token: String,
+        serverURL: String,
+        loggingEnabled: Bool
+    ) {
+        self.token = token
+        self.serverURL = serverURL
+        self.loggingEnabled = loggingEnabled
+    }
 }
